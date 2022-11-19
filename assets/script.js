ea4a8556a054ea0f56b370180e03c61a0f2360db
kazuki <kaz.tokyo.tech20@gmail.com>
2022-11-19 19:32:24 +0900

    add confirmation alert

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 /* placeholder file for JavaScript */
+const confirm_delete = ((id) => {
+  if (window.confirm('Are you sure you want to delete this?')) {
+    location.href = `/task/${id}/delete`;
+  }
+})
+
+const confirm_update = ((id) => {
+  if (window.confirm('Are you sure you want to update this?')) {
+    location.href = `/task/${id}/edit`;
+  }
+})
